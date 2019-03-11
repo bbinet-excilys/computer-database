8ec80a753795d33b5c91f1fbf4a9ffb689a5b6f9
Bastien Binet <bbinet@excilys.com>
2019-03-11 10:01:27 +0100

    Creating Computer Model Class

---
+++ src/model/Computer.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.sql.Date;
+
+public class Computer {
+
+    private Integer id, companyId;
+    private String  name;
+    private Date    introduced, discontinued;
+
+    public Computer() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getIntroduced() {
+        return introduced;
+    }
+
+    public void setIntroduced(Date introduced) {
+        this.introduced = introduced;
+    }
+
+    public Date getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(Date discontinued) {
+        this.discontinued = discontinued;
+    }
+
+}
