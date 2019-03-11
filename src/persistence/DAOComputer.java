5e8b044ee95e6af2a835f8cb40bb0593948d0b41
Bastien Binet <bbinet@excilys.com>
2019-03-11 10:34:38 +0100

    DAO Computer implementation

--- src/persistence/DAOComputer.java
+++ src/persistence/DAOComputer.java
@@ -1,6 +1,9 @@
 package persistence;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import model.Computer;
 
@@ -12,25 +15,132 @@ public class DAOComputer extends DAO<Computer> {
 
     @Override
     public boolean create(Computer object) {
-        // TODO Auto-generated method stub
+        PreparedStatement mPreparedStatement = null;
+        try {
+            mPreparedStatement = this.dbConnection.prepareStatement(
+                    "INSERT INTO computer(name, introduced, discontinued, company_id) VALUES (?,?,?,?,?);");
+            mPreparedStatement.setString(1, object.getName());
+            mPreparedStatement.setDate(2, object.getIntroduced());
+            mPreparedStatement.setDate(3, object.getDiscontinued());
+            mPreparedStatement.setInt(4, object.getCompanyId());
+            mPreparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        finally {
+            if (mPreparedStatement != null) {
+                try {
+                    mPreparedStatement.close();
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public Computer read(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        Computer          rComputer          = null;
+        PreparedStatement mPreparedStatement = null;
+        ResultSet         mResultSet         = null;
+        try {
+            mPreparedStatement = dbConnection
+                    .prepareStatement("SELECT id, name, introduced, discontinued, company_id FROM computer WHERE id=?");
+            mPreparedStatement.setInt(1, id);
+            mResultSet = mPreparedStatement.executeQuery();
+            mResultSet.first();
+            rComputer = new Computer();
+            rComputer.setId(mResultSet.getInt("id"));
+            rComputer.setName(mResultSet.getString("name"));
+            rComputer.setIntroduced(mResultSet.getDate("introduced"));
+            rComputer.setDiscontinued(mResultSet.getDate("discontinued"));
+            rComputer.setCompanyId(mResultSet.getInt("company_id"));
+        }
+        catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                mPreparedStatement.close();
+            }
+            catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+                mResultSet.close();
+            }
+            catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return rComputer;
     }
 
     @Override
     public boolean update(Computer object) {
-        // TODO Auto-generated method stub
+        PreparedStatement mPreparedStatement = null;
+        try {
+            mPreparedStatement = dbConnection.prepareStatement(
+                    "UPDATE computer SET name=?, introduced=?, discontinued=?, company_id=? WHERE ID=?");
+            mPreparedStatement.setString(1, object.getName());
+            mPreparedStatement.setDate(2, object.getIntroduced());
+            mPreparedStatement.setDate(3, object.getDiscontinued());
+            mPreparedStatement.setInt(4, object.getCompanyId());
+            mPreparedStatement.setInt(5, object.getId());
+            int status = mPreparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally {
+            if (mPreparedStatement != null) {
+                try {
+                    mPreparedStatement.close();
+                }
+                catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean delete(Computer object) {
-        // TODO Auto-generated method stub
+        PreparedStatement mPreparedStatement = null;
+        try {
+            mPreparedStatement = dbConnection.prepareStatement("DELETE FROM computer WHERE id=?");
+            mPreparedStatement.setInt(1, object.getId());
+            int status = mPreparedStatement.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally {
+            if (mPreparedStatement != null) {
+                try {
+                    mPreparedStatement.close();
+                }
+                catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
         return false;
     }
 
