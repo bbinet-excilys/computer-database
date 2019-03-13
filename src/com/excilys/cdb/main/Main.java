708da4fa81f561c00d27bec99af5dc2994bc3216
Bastien Binet <bbinet@excilys.com>
2019-03-13 10:14:23 +0100

    Update Main to use new Menu controller

--- src/com/excilys/cdb/main/Main.java
+++ src/com/excilys/cdb/main/Main.java
@@ -1,6 +1,8 @@
 package com.excilys.cdb.main;
 import com.excilys.cdb.main.control.CompanyController;
 import com.excilys.cdb.main.control.ComputerController;
+import com.excilys.cdb.main.control.ControllerFactory;
+import com.excilys.cdb.main.control.MenuController;
 import com.excilys.cdb.main.persistence.JDBCSingleton;
 import com.excilys.cdb.main.ui.UIHelper;
 import org.slf4j.Logger;
@@ -18,49 +20,8 @@ public class Main {
 
     public static void main(String[] args) {
         LOG.trace("Starting main method");
-        boolean loop = true;
-        ComputerController mComputerController = new ComputerController();
-        CompanyController  mCompanyController  = new CompanyController();
-        do {
-            UIHelper.displayMenu();
-            Integer choice;
-            do {
-                choice = UIHelper.promptInt("Please choose an item :");
-            } while (choice == null);
-            switch (choice) {
-            case 0:
-                loop = false;
-                break;
-            case 1:
-                mCompanyController.list();
-                break;
-            case 2:
-                mComputerController.list();
-                break;
-            case 3:
-                mComputerController.create();
-                break;
-            case 4:
-                mComputerController.read();
-                break;
-            case 5:
-                mComputerController.update();
-                break;
-            case 6:
-                mComputerController.delete();
-                break;
-            case 7:
-                mComputerController.pagedList();
-                break;
-            case 8:
-                mCompanyController.pagedList();
-                break;
-
-            default:
-                UIHelper.displayError("Not available");
-                break;
-            }
-        } while (loop);
+        MenuController menuController = (MenuController) ControllerFactory.INSTANCE.getController(ControllerFactory.MENU_CONTROLLER);
+        menuController.runMenu();
         JDBCSingleton.INSTANCE.closeConnection();
     }
 
