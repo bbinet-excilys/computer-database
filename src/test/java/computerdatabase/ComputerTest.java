580f00d6539bef6a7aafdedbc5e0fc8b3ee2d693
Bastien Binet <bbinet@excilys.com>
2019-03-13 16:59:49 +0100

    Create test folder to start testing classes

---
+++ src/test/java/computerdatabase/ComputerTest.java
@@ -0,0 +1,14 @@
+package computerdatabase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
