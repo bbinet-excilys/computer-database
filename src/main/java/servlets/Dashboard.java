7de92fc35dd28cf781020b446767907da6919e2d
Bastien Binet <bbinet@excilys.com>
2019-03-19 17:42:44 +0100

    Add servlets

--- src/main/java/servlets/Dashboard.java
+++ src/main/java/servlets/Dashboard.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,21 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.Computer;
-import model.ComputerPage;
-import persistence.DAOFactory;
-
 /**
- * Servlet implementation class ComputerServlet
+ * Servlet implementation class Dashboard
  */
-@WebServlet("/ComputerServlet")
-public class ComputerServlet extends HttpServlet {
+@WebServlet("/Dashboard")
+public class Dashboard extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   /**
    * @see HttpServlet#HttpServlet()
    */
-  public ComputerServlet() {
+  public Dashboard() {
     super();
     // TODO Auto-generated constructor stub
   }
@@ -34,18 +29,8 @@ public class ComputerServlet extends HttpServlet {
    */
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    List<Computer> computers = DAOFactory.INSTANCE.getDao(DAOFactory.DAO_COMPUTER).list();
-    String         sPage     = request.getParameter("page");
-    Integer        page      = 1;
-    if (sPage != null && !sPage.trim().isEmpty()) {
-      page = Integer.parseInt(sPage);
-    }
-    ComputerPage cPage = new ComputerPage();
-    cPage.setPageSize(10);
-    computers = cPage.getPageN(page);
-    request.setAttribute("computers", computers);
-    request.setAttribute("page", page);
-    getServletContext().getRequestDispatcher("/Views/dashboard.jsp").forward(request, response);
+    // TODO Auto-generated method stub
+    response.getWriter().append("Served at: ").append(request.getContextPath());
   }
 
   /**
